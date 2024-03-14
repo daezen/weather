14be4dbbd4a1304c5354552390c1d1e4a9567f23
daezen <me@daezen.co>
2024-03-13 23:16:31 -0500

    Add background image and reset

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 // const searchQuery = 'Tunja'
-const searchQuery = 'Tunja'
-const forecast = `http://api.weatherapi.com/v1/forecast.json?key=f1161324e1f843feb8923721241403&q=${searchQuery}&days=3&aqi=no&alerts=no`
+const $form = document.querySelector('[data-submit]')
+const $input = document.querySelector('[data-input]')
+const $city = document.querySelector('[data-city]')
+const $country = document.querySelector('[data-country]')
+$form.addEventListener('submit', handleSubmit)
 
-try {
-  const response = await fetch(forecast)
-  const result = JSON.parse(await response.text())
-} catch (error) {
-  console.error('Error')
+function handleSubmit(e) {
+  e.preventDefault()
+  fetchQuery($input.value)
+  $input.value = ''
+}
+
+async function fetchQuery(query) {
+  const forecast = `https://api.weatherapi.com/v1/forecast.json?key=f1161324e1f843feb8923721241403&q=${query}&days=3&aqi=no&alerts=no`
+  try {
+    const response = await fetch(forecast)
+    const result = JSON.parse(await response.text())
+    console.log(result)
+  } catch (error) {
+    console.error('Error')
+  }
 }
